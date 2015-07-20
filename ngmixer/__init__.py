8a620c7051d1ad2736637e237a62b25a98acf50b
Matthew R Becker <becker.mr@gmail.com>
2015-07-20 16:00:00 -0700

    imports

--- ngmixer/__init__.py
+++ ngmixer/__init__.py
@@ -1,1 +1,10 @@
-from . import *
+from . import collate
+from . import collate_general
+from . import config
+from . import constants
+from . import files
+from . import fitting
+from . import imageio
+from . import priors
+from . import util
+
