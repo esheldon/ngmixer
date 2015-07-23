1f16fc75eff088bc5a3cd28a8b3b30665d62825e
Matthew R Becker <becker.mr@gmail.com>
2015-07-23 12:18:07 -0700

    move specific pars to subclasses

--- ngmixer/fitting.py
+++ ngmixer/fitting.py
@@ -40,10 +40,10 @@ class NGMixER(dict):
         # build data
         if self.checkpoint_data is None:
             self.data = []
-            self.data_dtype = self._get_data_dtype()
+            self.data_dtype = self._get_dtype()
             self.epoch_data = []
-            self.epoch_data_dtype = self.get_epoch_data_dtype()
-        
+            self.epoch_data_dtype = self._get_epoch_dtype()
+
     def _set_some_defaults(self):
         self['fit_models'] = list(self['model_pars'].keys())
         self['max_box_size']=self.get('max_box_size',2048)
@@ -58,17 +58,11 @@ class NGMixER(dict):
     def get_epoch_data(self):
         return numpy.array(self.epoch_data,dtype=self.epoch_data_dtype)
         
-    def _check_models(self):
-        """
-        make sure all models are supported
-        """
-        for model in self['fit_models']:
-            if model not in ['exp','dev','cm']:
-                raise ValueError("model '%s' not supported" % model)
-
     def _get_all_models(self):
         """
         get all model names, includeing the coadd_ ones
+        
+        might be useful for all fitters, but not used here
         """
         models=[]
         
@@ -84,162 +78,27 @@ class NGMixER(dict):
         """
         makes epoch dtype
         """
-        psf_ngauss=self['psf_em_pars']['ngauss']
-        npars=psf_ngauss*6
-        dt=[('npix','i4'),
-            ('wsum','f8'),
-            ('wmax','f8'),
-            ('psf_fit_flags','i4'),
-            ('psf_counts','f8'),
-            ('psf_fit_g','f8',2),
-            ('psf_fit_T','f8'),
-            ('psf_fit_pars','f8',npars)]
-        dt += self.imageio.get_epoch_meta_data_dtype()
-
+        dt = self.imageio.get_epoch_meta_data_dtype()
         return dt
     
     def _make_epoch_struct(self,ncutout=1):
         """
         returns ncutout epoch structs to be filled
         """
-        epoch_data = numpy.zeros(ncutout, dtype=self.epoch_data_dtype)
-        epoch_data['npix'] = DEFVAL
-        epoch_data['wsum'] = DEFVAL
-        epoch_data['wmax'] = DEFVAL
-        epoch_data['psf_counts'] = DEFVAL
-        epoch_data['psf_fit_g'] = DEFVAL
-        epoch_data['psf_fit_T'] = DEFVAL
-        epoch_data['psf_fit_pars'] = DEFVAL
-        epoch_data['psf_fit_flags'] = NO_ATTEMPT
-        
+        dt = self._get_epoch_dtype()
+        epoch_data = numpy.zeros(ncutout, dtype=dt)
         return epoch_data
     
     def _get_dtype(self):
-        self._check_models()
-
-        nband=self['nband']
-        bshape=(nband,)
-        simple_npars=5+nband
-
-        dt=[('processed','i1'),
-            ('flags','i4'),
-            ('nimage_tot','i4',bshape),
-            ('nimage_use','i4',bshape),
-            ('time','f8'),
-
-            ('box_size','i2'),
-
-            ('coadd_npix','i4'),
-            ('coadd_mask_frac','f8'),
-            ('coadd_psfrec_T','f8'),
-            ('coadd_psfrec_g','f8', 2),
-
-            ('mask_frac','f8'),
-            ('psfrec_T','f8'),
-            ('psfrec_g','f8', 2)
-        ]
-        dt += self.imageio.get_meta_data_dtype()
-        
-        # the psf flux fits are done for each band separately
-        for name in ['coadd_psf','psf']:
-            n=Namer(name)
-            dt += [(n('flags'),   'i4',bshape),
-                   (n('flux'),    'f8',bshape),
-                   (n('flux_err'),'f8',bshape),
-                   (n('chi2per'),'f8',bshape),
-                   (n('dof'),'f8',bshape)]
-
-        if nband==1:
-            fcov_shape=(nband,)
-        else:
-            fcov_shape=(nband,nband)
-
-        models=self._get_all_models()
-        for model in models:
-            n=Namer(model)
-            np=simple_npars
-            
-            dt+=[(n('flags'),'i4'),
-                 (n('pars'),'f8',np),
-                 (n('pars_cov'),'f8',(np,np)),
-                 (n('flux'),'f8',bshape),
-                 (n('flux_cov'),'f8',fcov_shape),
-                 (n('g'),'f8',2),
-                 (n('g_cov'),'f8',(2,2)),
-                 
-                 (n('max_flags'),'i4'),
-                 (n('max_pars'),'f8',np),
-                 (n('max_pars_cov'),'f8',(np,np)),                 
-                 (n('chi2per'),'f8'),
-                 (n('dof'),'f8'),
-                 (n('s2n_w'),'f8'),
-            ]
-            
-            if self['do_shear']:
-                dt += [(n('g_sens'), 'f8', 2),
-                       (n('P'), 'f8'),
-                       (n('Q'), 'f8', 2),
-                       (n('R'), 'f8', (2,2))]
-            
+        dt = self.imageio.get_meta_data_dtype()
         return dt
-
+    
     def _make_struct(self,num=1):
         """
         make an output structure
         """
-        
-        data=numpy.zeros(num, dtype=self.data_dtype)
-
-        data['processed'] = 0
-        data['flags'] = NO_ATTEMPT        
-        data['nimage_tot'] = DEFVAL
-        data['nimage_use'] = DEFVAL
-        data['time'] = DEFVAL
-        data['box_size'] = DEFVAL
-
-        data['coadd_npix'] = DEFVAL
-        data['coadd_mask_frac'] = DEFVAL
-        data['coadd_psfrec_T'] = DEFVAL
-        data['coadd_psfrec_g'] = DEFVAL
-
-        data['mask_frac'] = DEFVAL
-        data['psfrec_T'] = DEFVAL
-        data['psfrec_g'] = DEFVAL
-        
-        for name in ['coadd_psf','psf']:
-            n=Namer(name)
-            data[n('flags')] = NO_ATTEMPT
-            data[n('flux')] = DEFVAL
-            data[n('flux_err')] = DEFVAL
-            data[n('chi2per')] = DEFVAL
-            data[n('dof')] = DEFVAL
-
-        models=self._get_all_models()
-        for model in models:
-            n=Namer(model)
-
-            data[n('flags')] = NO_ATTEMPT
-            
-            data[n('pars')] = DEFVAL
-            data[n('pars_cov')] = DEFVAL
-            data[n('flux')] = DEFVAL
-            data[n('flux_cov')] = DEFVAL
-            data[n('g')] = DEFVAL
-            data[n('g_cov')] = DEFVAL
-
-            data[n('s2n_w')] = DEFVAL
-            data[n('chi2per')] = DEFVAL
-
-            data[n('max_flags')] = NO_ATTEMPT
-            data[n('max_pars')] = DEFVAL
-            data[n('max_pars_cov')] = DEFVAL
-
-            if self['do_shear']:
-                data[n('g_sens')] = DEFVAL
-                data[n('P')] = DEFVAL
-                data[n('Q')] = DEFVAL
-                data[n('R')] = DEFVAL
-        
+        dt = self._get_dtype()
+        data=numpy.zeros(num, dtype=dt)
         return data
     
     def _setup_checkpoints(self):
