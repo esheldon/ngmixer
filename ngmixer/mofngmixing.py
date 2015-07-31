c73abd3b4bf188cdfa8d5ec6cf7a58b6877dcc20
Matthew R Becker <becker.mr@gmail.com>
2015-07-31 00:27:04 -0700

    hacked in convergence criterion - need to do better

--- ngmixer/mofngmixing.py
+++ ngmixer/mofngmixing.py
@@ -6,6 +6,7 @@ import time
 import pprint
 import os
 import fitsio
+import copy
 
 # local imports
 from . import imageio
@@ -15,11 +16,72 @@ from .ngmixing import NGMixer
 from .defaults import DEFVAL,LOGGERNAME,_CHECKPOINTS_DEFAULT_MINUTES
 from .defaults import NO_ATTEMPT,NO_CUTOUTS,BOX_SIZE_TOO_BIG,IMAGE_FLAGS
 from .util import UtterFailure
+from ngmix import print_pars
 
 # logging
 log = logging.getLogger(LOGGERNAME)
 
 class MOFNGMixer(NGMixer):
+    def _check_convergence(self,foflen):
+        """
+        check convergence of fits
+        """
+        
+        dlevel = 0
+        
+        npars = 5+self['nband']
+        maxabs = numpy.zeros(npars,dtype='f8')
+        maxabs[:] = -numpy.inf
+        maxfrac = numpy.zeros(npars,dtype='f8')
+        maxfrac[:] = -numpy.inf
+        maxerr = numpy.zeros(npars,dtype='f8')
+        maxerr[:] = -numpy.inf
+        
+        log.info('convergence:')
+        for fofind in xrange(foflen):
+            log.info('    fof obj: %ld' % fofind)
+
+            #FIXME - mofngmixer doesn't know what tags to use - prob get from fitter
+            for model in ['coadd_cm','cm']:
+                if model+'_pars' not in self.curr_data.dtype.names:
+                    continue
+                
+                old = self.prev_data[model+'_pars'][fofind]
+                new = self.curr_data[model+'_pars'][fofind]
+                absdiff = numpy.abs(new-old)
+                absfracdiff = numpy.abs(new/old-1.0)
+                abserr = numpy.abs((old-new)/numpy.sqrt(numpy.diag(self.curr_data[model+'_pars_cov'][fofind])))
+                
+                for i in xrange(npars):
+                    if absdiff[i] > maxabs[i]:
+                        maxabs[i] = copy.copy(absdiff[i])
+                    if absfracdiff[i] > maxfrac[i]:
+                        maxfrac[i] = copy.copy(absfracdiff[i])
+                    if abserr[i] > maxerr[i]:
+                        maxerr[i] = copy.copy(abserr[i])
+                
+                log.info('        %s:' % model)
+                print_pars(old,        front='            old      ')
+                print_pars(new,        front='            new      ')
+                print_pars(absdiff,    front='            abs diff ')
+                print_pars(absfracdiff,front='            frac diff')
+                print_pars(abserr,front='            err diff ')
+        
+                    
+        fmt = "%8.3g "*len(maxabs)
+        log.info("    max abs diff : "+fmt % tuple(maxabs))
+        log.info("    max frac diff: "+fmt % tuple(maxfrac))
+        log.info("    max err diff : "+fmt % tuple(maxerr))
+        
+        self.maxabs = maxabs
+        self.maxfrac = maxfrac
+        self.maxerr = maxerr
+        
+        if numpy.all((maxabs <= self['mof_maxabs_conv']) | (maxfrac <= self['mof_maxfrac_conv']) | (maxerr <= self['mof_maxerr_conv'])):
+            return True
+        else:
+            return False
+
     def do_fits(self):
         """
         Fit all objects in our list
@@ -41,12 +103,12 @@ class MOFNGMixer(NGMixer):
             foflen = len(mb_obs_lists)            
             log.info('    num in fof: %d' % foflen)
 
+            # deal with plots
             if self['make_plots']:
                 make_plots_at_end = True
+                self['make_plots'] = False
             else:
                 make_plots_at_end = False
-
-            self['make_plots'] = False
             
             # get data to fill
             self.curr_data = self._make_struct(num=foflen)
@@ -68,15 +130,21 @@ class MOFNGMixer(NGMixer):
 
                 self.curr_data_index += 1
 
+            if make_plots_at_end:
+                self['make_plots'] = True
+            
             # now fit again with nbrs - simple for now
-            Nitr = 1
-            for itr in xrange(Nitr):
-                # deal with plots
-                if itr == Nitr-1 and make_plots_at_end:
-                    self['make_plots'] = True
+            for itr in xrange(self['mof_max_itr']):
+                log.info('itr %d:' % itr)
+                
+                # data
+                self.prev_data = self.curr_data.copy()
 
-                self.curr_data_index = 0
-                for coadd_mb_obs_list,mb_obs_list in zip(coadd_mb_obs_lists,mb_obs_lists):
+                # fitting
+                for i in numpy.random.choice(foflen,size=foflen,replace=False):
+                    self.curr_data_index = i                                    
+                    coadd_mb_obs_list = coadd_mb_obs_lists[i]
+                    mb_obs_list = mb_obs_lists[i]
                     if foflen > 1:
                         log.info('fof obj: %d:%d' % (self.curr_data_index+1,foflen))
                     log.info('    id: %d' % mb_obs_list.meta['id'])
@@ -87,7 +155,8 @@ class MOFNGMixer(NGMixer):
                     ti = time.time()-ti
                     log.info('    time: %f' % ti)
                     
-                    self.curr_data_index += 1
+                if itr >= 1 and self._check_convergence(foflen):
+                    break
 
             # append data and incr.
             self.data.extend(list(self.curr_data))
