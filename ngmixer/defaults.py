051d97bb552b107e37fa9e7d38d9764c0df94c34
Matthew R Becker <becker.mr@gmail.com>
2015-12-11 13:20:07 -0800

    defined mof flags; moved some defaults

--- ngmixer/defaults.py
+++ ngmixer/defaults.py
@@ -1,5 +1,6 @@
 # flagging
-
+################################
+# bits for the 'flags' column
 # this is here for testing, it will be removed one day
 USE_OLD_FLAGS = False
 
@@ -23,31 +24,37 @@ else:
     UTTER_FAILURE        = 2**29
     NO_ATTEMPT           = 2**30
 
-
-
     # flags for fitting codes
     PSF_FIT_FAILURE      = 2**0
     GAL_FIT_FAILURE      = 2**1
     PSF_FLUX_FIT_FAILURE = 2**2
     LOW_PSF_FLUX         = 2**3
 
+################################
 # flags used by MOF 
-MOF_SKIPPED_IN_CONV_CHECK  = 2**0
-MOF_NOT_CONVERGED          = 2**1
+MOF_NBR_NOT_CONVERGED            = 2**0
+MOF_NBR_BAD_FIT                  = 2**1
+MOF_NBR_NOT_FIT                  = 2**2
+MOF_NBR_SKIPPED_IN_CONV_CHECK    = 2**3
+MOF_FOFMEM_NOT_CONVERGED         = 2**4
+MOF_FOFMEM_BAD_FIT               = 2**5
+MOF_FOFMEM_NOT_FIT               = 2**6
+MOF_FOFMEM_SKIPPED_IN_CONV_CHECK = 2**7
+MOF_NOT_CONVERGED                = 2**8
+MOF_SKIPPED_IN_CONV_CHECK        = 2**9
 
+################################
 # defaults
 DEFVAL = -9999
 PDEFVAL = 9999
 BIG_DEFVAL = -9.999e9
 BIG_PDEFVAL = 9.999e9
 
+################################
 # running code
-_CHECKPOINTS_DEFAULT_MINUTES=[0,30,60,110]
+_CHECKPOINTS_DEFAULT_MINUTES = [0,30,60,110]
 
-PSF_IND_FIELD='ind_psf'
-PSF_IM_FIELD='psf_im'
-
-#######################################################
+################################
 # setup logging/verbosity
 class _VERBOSITY(object):
     def __init__(self,level=0):
