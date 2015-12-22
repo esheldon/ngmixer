cd111a4bb5399fd842652f506f6cafd01a5398fa
Matthew R Becker <becker.mr@gmail.com>
2015-12-22 07:54:42 -0800

    added PSFs in file

--- ngmixer/imageio/simpsimmedsio.py
+++ ngmixer/imageio/simpsimmedsio.py
@@ -19,32 +19,40 @@ class SimpSimMEDSImageIO(MEDSImageIO):
         super(SimpSimMEDSImageIO, self)._set_defaults()
         self.conf['psf_ind_field'] = self.conf.get('psf_ind_field',PSF_IND_FIELD)
         self.conf['psf_im_field'] = self.conf.get('psf_im_field',PSF_IM_FIELD)
+        self.conf['psfs_in_file'] = self.conf.get('psfs_in_file',False)
 
     def _load_psf_data(self):
-        if 'psf_file' in self.extra_data:
-            self.psf_file = self.extra_data['psf_file']
-        else:
-            pth,bname = os.path.split(self.meds_files_full[0])
-            bname = bname.replace('meds','psf')
-            self.psf_file = os.path.join(pth,bname)
-        print('psf file: %s' % self.psf_file)
-        self.psf_data = fitsio.read(self.psf_file)
+        if not self.conf['psfs_in_file']:
+            if 'psf_file' in self.extra_data:
+                self.psf_file = self.extra_data['psf_file']
+            else:
+                pth,bname = os.path.split(self.meds_files_full[0])
+                bname = bname.replace('meds','psf')
+                self.psf_file = os.path.join(pth,bname)
+            print('psf file: %s' % self.psf_file)
+            self.psf_data = fitsio.read(self.psf_file)
 
     def _get_psf_image(self, band, mindex, icut):
         """
         Get an image representing the psf
         """
+        
+        if self.conf['psfs_in_file']:
+            im = self.meds_list[band].get_psf(mindex,icut)
+            pfile = self.meds_files[band]
+        else:
+            meds=self.meds_list[band]
+            psf_ind_field=self.conf['psf_ind_field']
+            
+            ind_psf = meds[psf_ind_field][mindex,icut]
+            
+            psf_im_field=self.conf['psf_im_field']
+            im = self.psf_data[psf_im_field][ind_psf].copy()
+            
+            pfile = self.psf_file
 
-        meds=self.meds_list[band]
-        psf_ind_field=self.conf['psf_ind_field']
-
-        ind_psf = meds[psf_ind_field][mindex,icut]
-
-        psf_im_field=self.conf['psf_im_field']
-        im = self.psf_data[psf_im_field][ind_psf].copy()
         im /= im.sum()
-
         cen = ( numpy.array(im.shape) - 1.0)/2.0
         sigma_pix = 2.5
-
-        return im, cen, sigma_pix, self.psf_file
+            
+        return im, cen, sigma_pix, pfile
