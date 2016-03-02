19295081bbbffbda0b079650994f756f05275c1c
Matthew R Becker <becker.mr@gmail.com>
2016-03-02 14:00:16 -0800

    made cake faster

--- ngmixer/megamixer/cakemegamixer.py
+++ ngmixer/megamixer/cakemegamixer.py
@@ -11,6 +11,35 @@ from .megamixer import NGMegaMixer
 import cake
 
 class CakeNGMegaMixer(NGMegaMixer):
+    def setup_coadd_tile(self,coadd_tile):
+        print("setting up tile '%s'" % coadd_tile)
+        files,fof_ranges = self.get_files_fof_ranges(coadd_tile)
+        
+        self._cake_chunks = []
+        self._cake_chunk_ids = []
+        
+        self.make_output_dirs(files,fof_ranges)
+        self.make_scripts(files,fof_ranges)
+
+        # add to cake
+        cake_file = os.path.join(files['work_output_dir'],'cake_chunks.db')
+        try:
+            os.remove(cake_file)
+        except:
+            pass
+        with cake.taskdb.SQLiteTaskDB(name=cake_file) as taskdb:
+            for cmd,id in zip(self._cake_chunks,self._cake_chunk_ids):
+                taskdb.add(cmd,id=id)
+
+        # add to global cake
+        cake_file = os.path.join(files['run_output_dir'],'cake_chunks.db')
+        with cake.taskdb.SQLiteTaskDB(name=cake_file) as taskdb:
+            for cmd,id in zip(self._cake_chunks,self._cake_chunk_ids):
+                try:
+                    taskdb.add(cmd,id=id)
+                except:
+                    pass
+
     def write_job_script(self,files,i,rng):
         fname = os.path.join(self.get_chunk_output_dir(files,i,rng),'job.sh')
         jobname = self.get_chunk_output_basename(files,self['run'],rng)
@@ -32,18 +61,10 @@ class CakeNGMegaMixer(NGMegaMixer):
 
         os.system('chmod 755 %s' % fname)
         
-        # add to cake
-        cake_file = os.path.join(files['work_output_dir'],'cake_chunks.db')
-        dr = self.get_chunk_output_dir(files,chunk,rng)
-        with cake.taskdb.SQLiteTaskDB(cake_file) as taskdb:
-            taskdb.add('cd %s && ./job.sh && cd -' % dr,id=jobname)
-
-        # add to global cake
-        cake_file = os.path.join(files['run_output_dir'],'cake_chunks.db')
-        dr = self.get_chunk_output_dir(files,chunk,rng)
-        with cake.taskdb.SQLiteTaskDB(cake_file) as taskdb:
-            taskdb.add('cd %s && ./job.sh && cd -' % dr,id=jobname)
-
+        dr = self.get_chunk_output_dir(files,i,rng)
+        self._cake_chunks.append('cd %s && ./job.sh && cd -' % dr)
+        self._cake_chunk_ids.append(jobname)
+        
     def run_chunk(self,files,chunk,rng):
         print "To run the chunks, build a job using cake (i.e., cake run /path/to/cake_chunks.db)"
 
@@ -68,16 +89,10 @@ class CakeNGMegaMixer(NGMegaMixer):
 
         os.system('chmod 755 %s' % fname)
 
-        # add to cake
-        cake_file = os.path.join(files['work_output_dir'],'cake_nbrs.db')
-        dr = files['work_output_dir']
-        with cake.taskdb.SQLiteTaskDB(cake_file) as taskdb:
-            taskdb.add('cd %s && ./jobnbrs.sh && cd -' % dr,id=jobname)
-
         # add to global cake
         cake_file = os.path.join(files['run_output_dir'],'cake_nbrs.db')
         dr = files['work_output_dir']
-        with cake.taskdb.SQLiteTaskDB(cake_file) as taskdb:
+        with cake.taskdb.SQLiteTaskDB(name=cake_file) as taskdb:
             taskdb.add('cd %s && ./jobnbrs.sh && cd -' % dr,id=jobname)
 
     def run_nbrs(self,files):
