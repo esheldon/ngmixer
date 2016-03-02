cf4cb5a606da453a3bd12f12c5ce775adc00e4f4
Matthew R Becker <becker.mr@gmail.com>
2016-03-02 13:37:10 -0800

    missed some imports; add to script

--- ngmixer/megamixer/__init__.py
+++ ngmixer/megamixer/__init__.py
@@ -2,3 +2,4 @@ from .megamixer import NGMegaMixer, BaseNGMegaMixer
 from .slacmegamixer import SLACNGMegaMixer
 from . import concat, desconcat
 from .concat_io import get_concat_class
+from .cakemegamixer import CakeNGMegaMixer
