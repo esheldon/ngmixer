3e60d4e6cba3ae1ced56efd506f924a9a20981db
Matthew R Becker <becker.mr@gmail.com>
2016-03-09 14:03:41 -0800

    added job arrays for LSF at SLAC

--- ngmixer/megamixer/__init__.py
+++ ngmixer/megamixer/__init__.py
@@ -1,5 +1,5 @@
 from .megamixer import NGMegaMixer, BaseNGMegaMixer
-from .slacmegamixer import SLACNGMegaMixer
+from .slacmegamixer import SLACNGMegaMixer, SLACArrayNGMegaMixer
 from . import concat, desconcat
 from .concat_io import get_concat_class
 from .cakemegamixer import CakeNGMegaMixer
