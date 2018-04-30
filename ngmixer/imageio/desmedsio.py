d6f1c9912cb7317e5bf871d8941840de4b3c67e0
Erin Sheldon <erin.sheldon@gmail.com>
2018-04-29 17:47:14 -0700

    I was using the wrong flag mapping
    turns out it wasn't use for Y3, only Y4+

--- ngmixer/imageio/desmedsio.py
+++ ngmixer/imageio/desmedsio.py
@@ -26,7 +26,7 @@ PSF_LOW_S2N=2**3
 PSF_FILE_READ_ERROR=2**4
 
 
-DES_BADPIX_MAP={
+DESY4_BADPIX_MAP={
     "BPM":          1,  #/* set in bpm (hot/dead pixel/column)        */
     "SATURATE":     2,  #/* saturated pixel                           */
     "INTERP":       4,  #/* interpolated pixel                        */
@@ -45,6 +45,24 @@ DES_BADPIX_MAP={
     "NEAREDGE":  8192,  #/* suspect due to edge proximity             */
     "TAPEBUMP": 16384,  #/* suspect due to known tape bump            */
 }
+
+DESY3_BADPIX_MAP={
+    "BPM":          1,  #/* set in bpm (hot/dead pixel/column)        */
+    "SATURATE":     2,  #/* saturated pixel                           */
+    "INTERP":       4,  #/* interpolated pixel                        */
+    "BADAMP":       8,  #/* Data from non-functional amplifier        */
+    "CRAY":        16,  #/* cosmic ray pixel                          */
+    "STAR":        32,  #/* bright star pixel                         */
+    "TRAIL":       64,  #/* bleed trail pixel                         */
+    "EDGEBLEED":  128,  #/* edge bleed pixel                          */
+    "SSXTALK":    256,  #/* pixel potentially effected by xtalk from  */
+                        #/*       a super-saturated source            */
+    "EDGE":       512,  #/* pixel flag to exclude CCD glowing edges   */
+    "STREAK":    1024,  #/* pixel associated with streak from a       */
+                        #/*       satellite, meteor, ufo...           */
+    "SUSPECT":   2048,  #/* nominally useful pixel but not perfect    */
+}
+#SUSPECT includes tapebump
 
 
 # SVMEDS
@@ -79,7 +97,7 @@ class SVDESMEDSImageIO(MEDSImageIO):
 
             flags = 0
             for flagname in flag_list:
-                flags += DES_BADPIX_MAP[flagname]
+                flags += DESY3_BADPIX_MAP[flagname]
 
             self.conf['extra_mask_flags'] = flags
 
@@ -294,7 +312,8 @@ class SVDESMEDSImageIO(MEDSImageIO):
         """
         Get an image representing the psf
         """
-        if self.conf['psfs_in_file']:
+        pconf=self.conf['imageio']['psfs']
+        if pconf['type']=='infile':
             return super(SVDESMEDSImageIO,self)._get_psf_image(
                 band, mindex, icut,
             )
@@ -715,7 +734,7 @@ class Y1DESMEDSImageIO(SVDESMEDSImageIO):
                 flag_list = pconf['ignore_when_set']
                 mask = 0
                 for flag in flag_list:
-                    mask += DES_BADPIX_MAP[flag]
+                    mask += DESY3_BADPIX_MAP[flag]
 
                 pconf['ignore_when_set_mask'] = mask
 
@@ -946,9 +965,9 @@ class Y1DESMEDSImageIO(SVDESMEDSImageIO):
         msk = self.conf['propagate_star_flags']['ignore_when_set_mask']
 
         is_sat_or_interp = (
-            (im1 & DES_BADPIX_MAP['SATURATE'] != 0) | (im1 & DES_BADPIX_MAP['INTERP'] != 0)
+            (im1 & DESY3_BADPIX_MAP['SATURATE'] != 0) | (im1 & DESY3_BADPIX_MAP['INTERP'] != 0)
         )
-        is_bright_star = (im1 & DES_BADPIX_MAP['STAR'] != 0)
+        is_bright_star = (im1 & DESY3_BADPIX_MAP['STAR'] != 0)
         not_other_bits = (im1 & msk == 0)
 
         q = numpy.where(
@@ -1084,7 +1103,7 @@ class Y1DESMEDSImageIO(SVDESMEDSImageIO):
 
     def _flag_y1_stellarhalo_masked_one(self,mb_obs_list):
 
-        starflag = DES_BADPIX_MAP['STAR']
+        starflag = DESY3_BADPIX_MAP['STAR']
 
         mindex = mb_obs_list.meta['meds_index']
         seg_number = self.meds_list[0]['number'][mindex]
@@ -1142,9 +1161,9 @@ class Y3DESMEDSImageIO(Y1DESMEDSImageIO):
     def __init__(self, *args, **kw):
 
         conf=args[0]
-        conf['psfs_in_file'] = conf.get('psfs_in_file',False)
+        pconf=conf['imageio']['psfs']
 
-        if not conf['psfs_in_file']:
+        if pconf['type'] == ['infile']:
             self._load_psf_map(**kw)
 
         super(Y3DESMEDSImageIO,self).__init__(*args, **kw)
@@ -1154,10 +1173,10 @@ class Y3DESMEDSImageIO(Y1DESMEDSImageIO):
         """
         load the psf blacklist
         """
-        pc=self.conf['imageio']['psfs']
+        pconf=self.conf['imageio']['psfs']
         blacklist={}
-        if 'blacklist' in pc:
-            fname=os.path.expandvars(pc['blacklist'])
+        if 'blacklist' in pconf:
+            fname=os.path.expandvars(pconf['blacklist'])
             print("reading psf blacklist:",fname)
             data=fitsio.read(fname)
             for d in data:
